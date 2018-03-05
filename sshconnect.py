2c3a3c972291d1fc19dd5fa63f17554dbf0d0f81
Rohan G Nair <rohangireeshnair@gmail.com>
2018-03-05 23:58:07 +0530

    Exceptions were added

--- sshconnect.py
+++ sshconnect.py
@@ -1,9 +1,15 @@
 import paramiko
 import getpass
-
+import sys
 def sshconnect(username, passwd, hostname, port):
     ssh_client=paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     ssh_client.load_system_host_keys()
-    ssh_client.connect(hostname, port, username, passwd)
+    try :
+        ssh_client.connect(hostname, port, username, passwd)
+    except paramiko.ssh_exception.AuthenticationException :
+        print('Authentication failure occured. Please check the following \n'
+              '1. Username/Password. \n'
+              '2. If ssh daemon is running on the target PC. \n')
+        sys.exit(0)
     return ssh_client
